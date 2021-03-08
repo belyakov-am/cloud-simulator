25fa73b08f7b0296b4d8ec70610e240cfd240032
Artem Belyakov <pochtaforfkn@gmail.com>
2021-03-08 13:32:15 +0300

    feat: extend task class

--- src/simulator/task.py
+++ src/simulator/task.py
@@ -1,7 +1,33 @@
+class File:
+    """
+    Representation for a file, that is used or produced by a task.
+    """
+
+    def __init__(
+            self,
+            name: str,
+            size: int,
+    ):
+        self.name = name
+        self.size = size
+
+
 class Task:
     """
     Representation for a task entity.
     """
 
-    def __init__(self):
-        pass
+    def __init__(
+            self,
+            task_id: int,
+            name: str,
+            parents: list["Task"],
+            input_files: list[File],
+            output_files: list[File],
+
+    ):
+        self.id = task_id
+        self.name = name
+        self.parents = parents
+        self.input_files = input_files
+        self.output_files = output_files
