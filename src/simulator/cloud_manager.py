d175a00206336d878414a2d0759090c298ff02d2
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-07 18:13:56 +0300

    feat: add simple executing method to cloud manager

--- src/simulator/cloud_manager.py
+++ src/simulator/cloud_manager.py
@@ -1,5 +1,6 @@
 import simulator.schedulers as sch
 import simulator.vm as vm
+import simulator.workflow as wf
 
 
 class CloudManager:
@@ -14,3 +15,9 @@ class CloudManager:
     def __init__(self, scheduler: sch.SchedulerInterface) -> None:
         self.scheduler: sch.SchedulerInterface = scheduler
         self.vm_manager: vm.VMManager = vm.VMManager()
+        self.workflows: dict[str, wf.Workflow] = dict()
+
+    def execute_workflow(self, workflow: wf.Workflow) -> None:
+        self.workflows[workflow.uuid] = workflow
+        self.scheduler.submit_workflow(workflow=workflow)
+        self.scheduler.schedule_workflow(workflow_uuid=workflow.uuid)
