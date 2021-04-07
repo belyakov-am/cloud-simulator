f615a9208bc9d56f45c283e5282b631c637acb57
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-07 18:13:23 +0300

    feat: add basic scheduler interface

--- src/simulator/schedulers/interface.py
+++ src/simulator/schedulers/interface.py
@@ -1,7 +1,35 @@
 from abc import ABC, abstractmethod
 
+import simulator.workflow as wf
+
 
 class SchedulerInterface(ABC):
+    """Interface for implementing scheduling algorithms"""
+
+    def __init__(self):
+        self.workflows: dict[str, wf.Workflow] = dict()
+
     @abstractmethod
-    def do_something(self):
+    def submit_workflow(self, workflow: wf.Workflow) -> None:
+        """This method can be used for any preprocessing required by
+        an algorithm. If an algorithm doesn't require any initial setup,
+        one should save given workflow to class instance variable
+        `workflows`
+
+        :param workflow: workflow for saving and preprocessing
+        :return: None
+        """
+
         pass
+
+    @abstractmethod
+    def schedule_workflow(self, workflow_uuid: str) -> None:
+        """Schedules given workflow according to algorithm's policy.
+
+        :param workflow_uuid: UUID of a workflow to schedule
+        :return: None
+        """
+
+        pass
+
+    # TODO: add method(s) for getting workflow status
