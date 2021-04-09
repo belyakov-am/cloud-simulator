0752a8b4272358c179dfe4b2a94e3a2cf27f1ecf
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-09 14:10:50 +0300

    feat: add simple storage

---
+++ src/simulator/storage/instance.py
@@ -0,0 +1,22 @@
+class Storage:
+    """Representation of a Simple Storage Service (S3).
+    For simplicity, each cloud manager has only one storage service.
+    This storage is used for sharing input and output files of tasks.
+    It is assumed that a storage has a sufficient capacity for handling
+    any amount of data.
+    """
+
+    def __init__(
+            self,
+            read_rate: float,
+            write_rate: float,
+    ):
+        """
+
+        :param read_rate: read rate from storage. Measures in megabits
+        per second (Mbps).
+        :param write_rate: write rate to storage. Measures in megabits
+        per second (Mbps).
+        """
+        self.read_rate = read_rate
+        self.write_rate = write_rate
