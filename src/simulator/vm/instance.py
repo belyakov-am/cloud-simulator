f4a3e98c477495d09cf8d67131470575fd80e01a
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-09 14:21:48 +0300

    feat: update VM init

--- src/simulator/vm/instance.py
+++ src/simulator/vm/instance.py
@@ -6,7 +6,6 @@ class VM:
             mips: float,
             processor_cores: int,
             io_bandwidth: int,
-            storage_capacity: int,
     ):
         """
         
@@ -15,7 +14,8 @@ class VM:
         core has power of `mips` (it is assumed that cores are equal).
         :param io_bandwidth: bandwidth capacity of I/O operations with
         other VMs. Measures in megabits per second (Mbps).
-        :param storage_capacity: VM's disk storage in megabytes (MB).
         """
 
-        pass
+        self.mips = mips
+        self.processor_cores = processor_cores
+        self.io_bandwidth = io_bandwidth
