273be0fbf0dfdc0a90b85cf8f5fc44dc86ede7d9
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-09 14:09:29 +0300

    style: fix docstring

--- src/simulator/vm/instance.py
+++ src/simulator/vm/instance.py
@@ -14,7 +14,7 @@ class VM:
         :param processor_cores: number of processor cores in VM. Each 
         core has power of `mips` (it is assumed that cores are equal).
         :param io_bandwidth: bandwidth capacity of I/O operations with
-        other VMs. Measure in megabits per second (Mbps).
+        other VMs. Measures in megabits per second (Mbps).
         :param storage_capacity: VM's disk storage in megabytes (MB).
         """
 
