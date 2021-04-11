b229ed6d2b1a431b4e67a4ca44e40aad06ac2cbf
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-11 22:10:54 +0300

    fix: KB to Mb convert

--- src/simulator/utils/task_execution_prediction.py
+++ src/simulator/utils/task_execution_prediction.py
@@ -16,23 +16,23 @@ def io_consumption(
     for input_file in task.input_files:
         # time for VM to read a file
         read_time += (input_file.size
-                      * KILOBYTE_TO_MEGABIT
+                      / KILOBYTE_TO_MEGABIT
                       / vm_instance.io_bandwidth)
 
         # time for storage to process a file
         read_time += (input_file.size
-                      * KILOBYTE_TO_MEGABIT
+                      / KILOBYTE_TO_MEGABIT
                       / storage.read_rate)
 
     for output_file in task.output_files:
         # time for VM to write a file
         read_time += (output_file.size
-                      * KILOBYTE_TO_MEGABIT
+                      / KILOBYTE_TO_MEGABIT
                       / vm_instance.io_bandwidth)
 
         # time for storage to process a file
         read_time += (output_file.size
-                      * KILOBYTE_TO_MEGABIT
+                      / KILOBYTE_TO_MEGABIT
                       / storage.write_rate)
 
     return read_time
