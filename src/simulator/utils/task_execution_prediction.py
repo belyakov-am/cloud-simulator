1f96e9d072f31204ffa47f388c57a09eb2189e97
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-12 17:11:36 +0300

    refactor: rename variable

--- src/simulator/utils/task_execution_prediction.py
+++ src/simulator/utils/task_execution_prediction.py
@@ -11,20 +11,20 @@ def io_consumption(
         vm_instance: vm.VM,
         storage: st.Storage
 ) -> float:
-    read_time = 0.0
+    total_time = 0.0
 
     for input_file in task.input_files:
         # time for VM to read a file
-        read_time += input_file.size_in_megabits() / vm_instance.io_bandwidth
+        total_time += input_file.size_in_megabits() / vm_instance.io_bandwidth
 
         # time for storage to process a file
-        read_time += input_file.size_in_megabits() / storage.read_rate
+        total_time += input_file.size_in_megabits() / storage.read_rate
 
     for output_file in task.output_files:
         # time for VM to write a file
-        read_time += output_file.size_in_megabits() / vm_instance.io_bandwidth
+        total_time += output_file.size_in_megabits() / vm_instance.io_bandwidth
 
         # time for storage to process a file
-        read_time += output_file.size_in_megabits() / storage.write_rate
+        total_time += output_file.size_in_megabits() / storage.write_rate
 
-    return read_time
+    return total_time
