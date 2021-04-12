7a79ed803f4587faa849e6b04392f692cdc56db6
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-12 14:35:30 +0300

    refactor: rename constant

--- src/simulator/utils/task_execution_prediction.py
+++ src/simulator/utils/task_execution_prediction.py
@@ -3,7 +3,7 @@ import simulator.vm as vm
 import simulator.workflow as wf
 
 
-KILOBYTE_TO_MEGABIT = 125
+KILOBYTES_IN_MEGABIT = 125
 
 
 def io_consumption(
@@ -16,23 +16,23 @@ def io_consumption(
     for input_file in task.input_files:
         # time for VM to read a file
         read_time += (input_file.size
-                      / KILOBYTE_TO_MEGABIT
+                      / KILOBYTES_IN_MEGABIT
                       / vm_instance.io_bandwidth)
 
         # time for storage to process a file
         read_time += (input_file.size
-                      / KILOBYTE_TO_MEGABIT
+                      / KILOBYTES_IN_MEGABIT
                       / storage.read_rate)
 
     for output_file in task.output_files:
         # time for VM to write a file
         read_time += (output_file.size
-                      / KILOBYTE_TO_MEGABIT
+                      / KILOBYTES_IN_MEGABIT
                       / vm_instance.io_bandwidth)
 
         # time for storage to process a file
         read_time += (output_file.size
-                      / KILOBYTE_TO_MEGABIT
+                      / KILOBYTES_IN_MEGABIT
                       / storage.write_rate)
 
     return read_time
