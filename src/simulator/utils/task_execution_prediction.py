9f775587d8ab48fde9dfe0db051d1a53329cbdc3
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-12 16:47:11 +0300

    refactor: switch to method

--- src/simulator/utils/task_execution_prediction.py
+++ src/simulator/utils/task_execution_prediction.py
@@ -15,24 +15,16 @@ def io_consumption(
 
     for input_file in task.input_files:
         # time for VM to read a file
-        read_time += (input_file.size
-                      / KILOBYTES_IN_MEGABIT
-                      / vm_instance.io_bandwidth)
+        read_time += input_file.size_in_megabits() / vm_instance.io_bandwidth
 
         # time for storage to process a file
-        read_time += (input_file.size
-                      / KILOBYTES_IN_MEGABIT
-                      / storage.read_rate)
+        read_time += input_file.size_in_megabits() / storage.read_rate
 
     for output_file in task.output_files:
         # time for VM to write a file
-        read_time += (output_file.size
-                      / KILOBYTES_IN_MEGABIT
-                      / vm_instance.io_bandwidth)
+        read_time += output_file.size_in_megabits() / vm_instance.io_bandwidth
 
         # time for storage to process a file
-        read_time += (output_file.size
-                      / KILOBYTES_IN_MEGABIT
-                      / storage.write_rate)
+        read_time += output_file.size_in_megabits() / storage.write_rate
 
     return read_time
