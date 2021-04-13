8dcc41a97dafcca9020c504552d3495b717b7440
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-14 00:12:54 +0300

    feat: add VM types

--- src/simulator/config.py
+++ src/simulator/config.py
@@ -3,3 +3,6 @@ import pathlib
 
 SRC_DIR = pathlib.Path(__file__).parent.parent.absolute()
 LOGS_DIR = str(SRC_DIR / "logs")
+
+RESOURCES_DIR = SRC_DIR / "resources"
+VM_TYPES = str(RESOURCES_DIR / "vms.json")
--- src/simulator/vms/manager.py
+++ src/simulator/vms/manager.py
@@ -1,3 +1,6 @@
+import json
+
+import simulator.config as config
 import simulator.vms as vms
 
 
@@ -5,11 +8,36 @@ class Manager:
     """VM Manager"""
 
     def __init__(self):
+        # List of VM types. Sorted by price in ascending order.
+        self.vm_types: list[vms.VM] = []
         self.vms: list[vms.VM] = []
 
-    # TODO: implement
-    def get_slowest_vm(self):
-        return vms.VM(
-            cpu=1,
-            io_bandwidth=100000,
+        self._get_vm_types()
+
+    def _get_vm_types(self):
+        with open(config.VM_TYPES) as f:
+            json_data = json.load(f)
+
+        json_vms = json_data["vms"]
+        for json_vm in json_vms:
+            vm = vms.VM(
+                name=json_vm["name"],
+                cpu=json_vm["cpu"],
+                memory=json_vm["memory"],
+                price_per_hour=json_vm["pricePerHour"],
+                io_bandwidth=json_vm["IOBandwidth"],
+            )
+            self.vm_types.append(vm)
+
+        self.vm_types = sorted(
+            self.vm_types,
+            key=lambda v: v.price_per_hour,
         )
+
+    def get_slowest_vm(self):
+        # Slowest VM is defined by its CPU
+        # WARNING: in default setup slowest VM is the cheapest one.
+        # One my change possible VM types and maybe need to change this
+        # logic
+
+        return self.vm_types[0]
