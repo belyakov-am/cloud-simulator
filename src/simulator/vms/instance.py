30264fe4923075815d74dcd87ed6cd196fa0d550
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-13 21:40:56 +0300

    feat: change VM instance fields

--- src/simulator/vms/instance.py
+++ src/simulator/vms/instance.py
@@ -3,19 +3,16 @@ class VM:
 
     def __init__(
             self,
-            mips: float,
-            processor_cores: int,
+            cpu: int,
             io_bandwidth: int,
     ):
         """
         
-        :param mips: Millions of Instruction Per Second. 
-        :param processor_cores: number of processor cores in VM. Each 
+        :param cpu: number of virtual processor cores in VM. Each
         core has power of `mips` (it is assumed that cores are equal).
         :param io_bandwidth: bandwidth capacity of I/O operations with
         other VMs. Measures in megabits per second (Mbps).
         """
 
-        self.mips = mips
-        self.processor_cores = processor_cores
+        self.cpu = cpu
         self.io_bandwidth = io_bandwidth
