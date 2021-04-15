44d9e225e1de4c6978048e8c86bb76d2e229f713
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-15 11:45:28 +0300

    fix: add workflow uuid to container creation

--- src/simulator/workflows/instance.py
+++ src/simulator/workflows/instance.py
@@ -24,7 +24,7 @@ class Workflow:
         self.dag: nx.DiGraph = nx.DiGraph()
 
         # Container that simulates required libraries and software
-        self.container: wfs.Container = wfs.Container(0)
+        self.container: wfs.Container = wfs.Container(self.uuid, 0)
 
     def set_deadline(self, deadline: datetime):
         self.deadline = deadline
