31adfc02498247af0eac5244c7c718662f758ab3
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-17 17:34:16 +0300

    feat: add current time to event loop

--- src/simulator/schedulers/event_loop.py
+++ src/simulator/schedulers/event_loop.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import heapq as hq
 
 import simulator.schedulers as sch
@@ -12,12 +13,21 @@ class EventLoop:
         self.event_queue: list[sch.Event] = []
         hq.heapify(self.event_queue)
 
+        # datitime.now() only for init purpose
+        self.current_time: datetime = datetime.now()
+
     def add_event(self, event: sch.Event) -> None:
         hq.heappush(self.event_queue, event)
+
+    def get_current_time(self) -> datetime:
+        return self.current_time
 
     def run(self, scheduler: sch.SchedulerInterface) -> None:
         while self.event_queue:
             event: sch.Event = hq.heappop(self.event_queue)
+
+            # update current time
+            self.current_time = event.start_time
 
             if event.type == sch.EventType.SCHEDULE_WORKFLOW:
                 scheduler.schedule_workflow(event.workflow.uuid)
