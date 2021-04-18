ab3b2b9fbfa678ce48d12660f9c51c2322e55795
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-18 20:21:20 +0300

    clar: add missing dots

--- src/simulator/schedulers/event_loop.py
+++ src/simulator/schedulers/event_loop.py
@@ -13,7 +13,7 @@ class EventLoop:
         self.event_queue: list[sch.Event] = []
         hq.heapify(self.event_queue)
 
-        # datetime.now() only for init purpose
+        # datetime.now() only for init purpose.
         self.current_time: datetime = datetime.now()
 
     def add_event(self, event: sch.Event) -> None:
@@ -26,7 +26,7 @@ class EventLoop:
         while self.event_queue:
             event: sch.Event = hq.heappop(self.event_queue)
 
-            # update current time
+            # Update current time.
             self.current_time = event.start_time
 
             if event.type == sch.EventType.SCHEDULE_WORKFLOW:
