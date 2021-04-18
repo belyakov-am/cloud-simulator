9abcccffc96b6739c2bd7afa81055a5c33226b58
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-18 21:02:14 +0300

    feat: add check for existing files in execution prediction

--- src/simulator/utils/task_execution_prediction.py
+++ src/simulator/utils/task_execution_prediction.py
@@ -1,3 +1,5 @@
+import typing as tp
+
 import simulator.storages as sts
 import simulator.vms as vms
 import simulator.workflows as wfs
@@ -9,7 +11,8 @@ KILOBYTES_IN_MEGABIT = 125
 def io_consumption(
         task: wfs.Task,
         vm_type: vms.VMType,
-        storage: sts.Storage
+        storage: sts.Storage,
+        vm: tp.Optional[vms.VM],
 ) -> float:
     """Return prediction for time execution based on IO consumption.
     It includes time for process data over network and disk.
@@ -22,7 +25,12 @@ def io_consumption(
         total_time += input_file.size_in_megabits() / vm_type.io_bandwidth
 
         # Time for storage to process file.
-        total_time += input_file.size_in_megabits() / storage.read_rate
+        # If vm was given, check if file already on it, so no network
+        # transfer is required.
+        if (vm is None
+            or (vm is not None
+                and not vm.check_if_files_present(files=[input_file]))):
+            total_time += input_file.size_in_megabits() / storage.read_rate
 
     for output_file in task.output_files:
         # Time for VM to write file.
