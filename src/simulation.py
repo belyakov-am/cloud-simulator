185749b694c9895f9d38854ea5ef0d18f200993c
Artem Belyakov <pochtaforfkn@gmail.com>
2021-04-25 13:52:21 +0300

    feat: extend simulation with several workflow sets and both schedulers

--- src/simulation.py
+++ src/simulation.py
@@ -1,105 +1,139 @@
+from collections import defaultdict
 from datetime import datetime, timedelta
 import pathlib
 import typing as tp
 
 from loguru import logger
+from wfcommons import WorkflowGenerator
+from wfcommons.generator import GenomeRecipe, CyclesRecipe
 
 import simulator as sm
 import simulator.schedulers as sch
-import simulator.schedulers.epsm as epsm
 import simulator.workflows as wfs
 
 
 ROOT_DIR = pathlib.Path(__file__).parent.parent
-WORKFLOW_PATH = "workflow-traces/pegasus"
-TRACE_TYPES = [
-    "genome",
-    "montage",
-]
+WORKFLOW_DIR = (ROOT_DIR / "workflow-traces/pegasus/generated")
+
+DEFAULT_NUM_TASKS: list[int] = [20, 100, 500]
+DEFAULT_WORKFLOWS_PER_RECIPE = 10
+
+CONTAINER_PROV_DELAY: dict[str, int] = {
+    "Genome": 600,
+    "Cycles": 400,
+}
 
 
-def parse_single_workflow(
-        path: str,
-        deadline: tp.Optional[datetime] = None,
-        budget: tp.Optional[float] = None,
-) -> wfs.Workflow:
-    """Parse single workflow. If deadline or budget were given, set
-    them.
+def generate_workflows(
+        recipes: list[tp.Any],
+        num_tasks: tp.Optional[list[int]] = None,
+        workflows_per_recipe: int = DEFAULT_WORKFLOWS_PER_RECIPE,
+) -> None:
+    """Generate workflows using wfcommons tool. Produce
+    `workflow_per_recipe` workflows for each recipe for each number of
+    tasks. Save them to `WORKFLOW_DIR`.
 
-    :param path: path to json file with workflow's description.
-    :param deadline: deadline for workflow.
-    :param budget: budget for workflow.
-    :return: workflow instance.
+    :param recipes: recipes for generating workflows.
+    :param num_tasks: list of number of tasks that should be in workflow.
+    :param workflows_per_recipe: number of workflows per recipe.
+    :return: None.
     """
 
-    parser = wfs.PegasusTraceParser(path)
-    workflow = parser.get_workflow()
+    if num_tasks is None:
+        num_tasks = DEFAULT_NUM_TASKS
 
-    if deadline is not None:
+    for recipe in recipes:
+        for num_task in num_tasks:
+            for i in range(workflows_per_recipe):
+                rcp = recipe.from_num_tasks(num_tasks=num_task)
+                generator = WorkflowGenerator(workflow_recipe=rcp)
+                workflow = generator.build_workflow()
+
+                current_dir = WORKFLOW_DIR / f"{num_task}"
+                current_dir.mkdir(parents=True, exist_ok=True)
+
+                filename = str(
+                    current_dir / f"{workflow.name}-{num_task}-{i}.json"
+                )
+
+                workflow.name = f"{workflow.name}-{num_task}-{i}"
+                workflow.write_json(json_filename=filename)
+
+
+def parse_workflows() -> dict[int, dict[str, wfs.Workflow]]:
+    """Parse workflows from `WORKFLOW_DIR` to `Workflow` instances.
+
+    :return: map from num_task to map from workflow UUID to workflow
+    instance.
+    """
+
+    workflow_sets: dict[int, dict[str, wfs.Workflow]] = defaultdict(dict)
+
+    for trace_path in WORKFLOW_DIR.glob("**/*"):
+        if trace_path.is_dir():
+            continue
+
+        trace_path_str = str(trace_path)
+
+        container_prov = 0
+        for k, v in CONTAINER_PROV_DELAY.items():
+            if k in trace_path_str:
+                container_prov = v
+                break
+
+        parser = wfs.PegasusTraceParser(
+            filename=trace_path_str,
+            container_prov=container_prov,
+        )
+
+        workflow = parser.get_workflow()
+        num_tasks = int(trace_path.parent.name)
+
+        deadline = datetime.now() + timedelta(hours=num_tasks // 10)
         workflow.set_deadline(time=deadline)
 
-    if budget is not None:
+        budget = num_tasks
         workflow.set_budget(budget=budget)
 
-    return workflow
+        workflow_sets[num_tasks][workflow.uuid] = workflow
 
-
-def parse_workflows() -> dict[str, wfs.Workflow]:
-    """Parse workflows from directories.
-
-    :return: dict from workflow UUID to workflow object.
-    """
-
-    workflows: dict[str, wfs.Workflow] = dict()
-
-    for trace_type in TRACE_TYPES:
-        trace_type_dir = str(ROOT_DIR / WORKFLOW_PATH / trace_type)
-        for trace_path in pathlib.Path(trace_type_dir).glob("**/*"):
-            workflow = parse_single_workflow(
-                path=str(trace_path),
-                deadline=datetime.now() + timedelta(hours=12),
-            )
-            workflows[workflow.uuid] = workflow
-
-    return workflows
+    return workflow_sets
 
 
 def main() -> None:
-    # Init workflows.
-    workflows = parse_workflows()
+    # generate_workflows(recipes=[GenomeRecipe, CyclesRecipe])
+    workflow_sets = parse_workflows()
 
-    # Choose scheduler.
-    scheduler = sch.EPSMScheduler()
-    settings = epsm.Settings()
-    scheduler.set_settings(settings=settings)
+    schedulers = [sch.EPSMScheduler(), sch.EBPSMScheduler()]
+    total_stats: dict[int, dict[str, sm.MetricCollector]] = defaultdict(dict)
 
-    # Create simulator, submit workflows and run simulation.
-    simulator = sm.Simulator(scheduler=scheduler)
+    for scheduler in schedulers:
+        for num_tasks, workflows in workflow_sets.items():
+            simulator = sm.Simulator(scheduler=scheduler)
 
-    for _, workflow in workflows.items():
-        simulator.submit_workflow(workflow=workflow, time=datetime.now())
+            for _, workflow in workflows.items():
+                simulator.submit_workflow(
+                    workflow=workflow,
+                    time=datetime.now(),
+                )
 
-    simulator.run_simulation()
+            simulator.run_simulation()
 
-    # Get simulation metrics.
-    metric_collector = simulator.get_metric_collector()
+            collector = simulator.get_metric_collector()
+            total_stats[num_tasks][scheduler.name] = collector
 
-    logger.info(f"Total cost = {metric_collector.cost}")
+    for num_tasks, scheduler_stats in total_stats.items():
+        for scheduler_name, stats in scheduler_stats.items():
+            exec_time = (stats.finish_time - stats.start_time).total_seconds()
 
-    for workflow_uuid, stats in metric_collector.workflows.items():
-        workflow = workflows[workflow_uuid]
-
-        total_seconds = (stats.finish_time - stats.start_time).total_seconds()
-
-        logger.info(
-            f"Workflow {workflow_uuid} statistics\n"
-            f"Start time = {stats.start_time}\n"
-            f"Finish time = {stats.finish_time}\n"
-            f"Deadline = {workflow.deadline}\n"
-            f"Execution time (seconds) = {total_seconds}\n"
-            f"Number of initialized VMs = {len(stats.initialized_vms)}\n"
-            f"Number of used VMs = {len(stats.used_vms)}\n"
-        )
+            logger.info(
+                f"Scheduler name = {scheduler_name}\n"
+                f"Number of tasks in workflows = {num_tasks}\n"
+                f"Total cost = {stats.cost}\n"
+                f"Total exec time = {exec_time}\n"
+                f"Start time = {stats.start_time}\n"
+                f"Finish time = {stats.finish_time}\n"
+            )
 
 
 if __name__ == '__main__':
