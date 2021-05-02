007c6acfe111062dc85dab0f6096618df95b8d9a
Artem Belyakov <pochtaforfkn@gmail.com>
2021-05-02 18:00:00 +0300

    feat: export Min-Min scheduler

--- src/simulator/schedulers/__init__.py
+++ src/simulator/schedulers/__init__.py
@@ -3,6 +3,7 @@ import simulator.schedulers.epsm
 from .dyna import DynaScheduler
 from .ebpsm import EBPSMScheduler
 from .epsm import EPSMScheduler, Settings
+from .minmin import MinMinScheduler
 
 from .event import Event, EventType
 from .event_loop import EventLoop
