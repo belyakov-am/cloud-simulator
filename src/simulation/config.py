6bdb916e0f3c26f9f241fd69516db2e803752751
Artem Belyakov <pochtaforfkn@gmail.com>
2021-05-09 17:09:19 +0300

    refactor: add type hints

--- src/simulation/config.py
+++ src/simulation/config.py
@@ -11,7 +11,7 @@ GRAPHICS_DIR = ROOT_DIR / "src" / "graphics"
 #   file transfer).
 #   - `io_and_runtime` -- considers both IO operations and task's
 #   runtime.
-PREDICT_EXEC_TIME_FUNC = "io_and_runtime"
+PREDICT_EXEC_TIME_FUNC: str = "io_and_runtime"
 
 # Indicates time required for VM manager to provision VM. For
 # simplicity, it is assumed that each VM requires same time to
@@ -23,7 +23,7 @@ VM_PROVISION_DELAY: int = 120
 # period, it can be shutdown.
 VM_DEPROVISION_PERCENT: float = 0.2
 # Billing periods for VMs in seconds.
-VM_BILLING_PERIODS = [
+VM_BILLING_PERIODS: list[int] = [
     3600,
     60,
     1,
@@ -32,26 +32,26 @@ VM_BILLING_PERIODS = [
 # List of num_tasks for execution.
 NUM_TASKS_EXECUTION: list[int] = [
     150,
-    300,
-    600,
+    # 300,
+    # 600,
 ]
 
 # Total number of workflows to generate by workflow pool.
-WORKFLOW_NUMBER = 150
+WORKFLOW_NUMBER: int = 150
 # Workload size i.e. number of workflows in one simulation.
-WORKLOAD_SIZE = [
+WORKLOAD_SIZE: list[int] = [
+    5,
     10,
-    20,
-    40,
+    15,
 ]
 # Interval for submitting workflows in even load.
-EVEN_LOAD_INTERVAL = 720
+EVEN_LOAD_INTERVAL: int = 720
 # Percent of workload size for submitting each interval.
-EVEN_LOAD_WORKFLOWS_PER_INTERVAL = 0.2
+EVEN_LOAD_WORKFLOWS_PER_INTERVAL: float = 0.2
 
 # Number of simulation per one series. Series is list of fixed
 # parameters for simulation (scheduler, workload size, billing period).
-SIMULATIONS_IN_SERIES = 10
+SIMULATIONS_IN_SERIES: int = 5
 
 # Container provisioning delay for different types of workflows.
 # Declared in seconds.
@@ -70,12 +70,12 @@ CONTAINER_PROV_DELAY: dict[str, int] = {
 # respectively if constraint is deadline, and they are execution costs
 # on slowest and fastest VM types respectively if constraint is budget.
 # Should fall in [0; 1).
-STEP_FROM_MIN_CONSTRAINT = 0.85
+STEP_FROM_MIN_CONSTRAINT: float = 0.85
 
-SEED = 42
+SEED: int = 42
 
 # Number of processes to start for executing simulations in series.
-PROCESS_NUMBER = 5
+PROCESS_NUMBER: int = 5
 
 # Number of workflows to parse. Used for testing.
-NUMBER_OF_WORKFLOWS = 10000000
+NUMBER_OF_WORKFLOWS: int = 10000000
